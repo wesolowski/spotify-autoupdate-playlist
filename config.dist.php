b3518c2779f04e3e3dc5adbffe07c11b66d2348e
Rafal Wesolowski <wesolowski@nexus-netsoft.com>
2018-05-08 23:27:58 +0200

    Init

---
+++ config.dist.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'clientId' => 'spotify_Client_Id',
+    'clientSecret' => 'spotify_Client_Secret',
+    'redirectUri' => 'spotify_redirect_uri',
+    'user' => 'spotify_user_id',
+    'playlist_name' => 'spotify_playlist_to_update',
+];
